5fda81a4f6951ec6f736df4f6cedf5cace045280
phandachuynh <phandachuynh123@gmail.com>
2021-11-15 14:39:31 +0700

    chuc nang ChanLe

--- source.cpp
+++ source.cpp
@@ -8,20 +8,32 @@ int hieu(int a, int b)
 {
 	return a+b;
 }
+int chanle(int x)
+{
+if(x%2==0)
+cout<<x<<"La so chan";
+else
+cout<<x<<"La so le
+}
 int main()
 {
-	int a, b;
+	int a, b, x;
 	char phepToan;
 	cout << "Nhap so nguyen 1: ";
 	cin >> a;
 	cout << "Nhap so nguyen 2: ";
 	cin >> b;
-	cout << "Chon phep toan (+,-,*,/) ";
+	cout << "Chon phep toan (+,-,*,/,) ";
 	cin >> phepToan;
+
 if(phepToan=="+"){
 	cout<<"Tong="<<tong(a,b);}
 if(phepToan=="-")
 	cout<<"Hieu="<<hieu(a,b);
+
+cout << "Nhap so bat ki x: ";
+	cin >> x;
+cout<<"X"<<chanle(x);
 	system("pause");
 	return 0;
 }
